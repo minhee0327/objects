3158bca1f60710b28a32fce74e9a4e3eecac391a
ddukda <ddukda@gmail.com>
2025-04-05 12:53:02 +0900

    ch1 project #1

---
+++ jino/week1/project/src/main/java/org/example/Audience.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Audience {
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
+
---
+++ jino/week1/project/src/main/java/org/example/Invitation.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+
+}

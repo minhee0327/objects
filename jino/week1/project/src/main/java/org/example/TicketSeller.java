16b6242041782fd584c59fe3158dd1d64f6efd35
ddukda <ddukda@gmail.com>
2025-04-05 13:07:09 +0900

    ch1 project #2

--- jino/week1/project/src/main/java/org/example/TicketSeller.java
+++ jino/week1/project/src/main/java/org/example/TicketSeller.java
@@ -8,7 +8,9 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        Long amount = audience.buy(ticketOffice.getTicket());
+        ticketOffice.plusAmount(amount);
     }
+
 }
